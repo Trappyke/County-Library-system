20b8a3685e8c35e2378776efc207691c31e0c042
Trappyke <98483628+Trappyke@users.noreply.github.com>
2022-03-01 20:05:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -8,22 +8,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void execute_action(int action);
 int menu(){
     int action;
         printf("Select An Action Below...\n");
         printf("1. Add New Patron\n");
         printf("2. View All Patrons\n");
         printf("3. Add New Resources\n");
-        printf("4. View All Resources\n");
+        printf("4. View All Resources\n\n");
         printf("Your Selection: \n");
         scanf("%d", &action);
+        if (action < 1 || action > 4){
+            printf("Invalid Action. Try Again!\n\n");
+        }
         return action;
 }
+
+void execute_action(int action) {
+    switch (action) {
+    case 1:
+        printf("Adding New Patron\n");
+        break;
+    case 2:
+        printf("Here is the list of Patron\n");
+        break;
+    case 3:
+        printf("Adding New Resources\n");
+        break;
+    case 4:
+        printf("Here Is the list of Resources\n");
+        break;
+    default:
+        printf("Wrong Entry\n");
+
+    }
+
+}
+
 int main(){
     int action;
-    printf("COUNTY LIBRARY SYSTEM\n");
-    printf("Welcome Trappy!\n");
+    char title[100] = "COUNTY LIBRARY MANAGEMENT";
+    char status[100] = "Welcome Weston";
+    printf("\n\n\n\t\t\t   *********************\n\t\t\t%s\n \t\t\t\    %s\n\t\t\t   ********************\n\n", title, status);
     action = menu();
-    printf("Your Selection: %d\n");
+    execute_action(menu());
     return 0;
 }
